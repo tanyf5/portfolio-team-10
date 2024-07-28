830b05b7c8c69c5f904a2b37fa9602e4a7a69dd8
Ihor Mikheyev <ihor.mikheiev@gmail.com>
2024-07-28 07:59:10 +0200

    rm bg

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream
 import './components/form-validation';
-=======
->>>>>>> Stashed changes
 import './components/form-storage';
 import './components/api';
 import './components/modal';
