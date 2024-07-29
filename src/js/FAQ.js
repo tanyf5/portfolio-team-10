e7a9ac545d49853eaa1040483ed1a759b5a5d4c7
tanyf5 <leehmann05@gmail.com>
2024-07-29 10:54:43 +0200

    faq from tetiana

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -2,28 +2,23 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Typed from 'typed.js';
 import spriteSvg from '../img/icons.svg';
-
 new Accordion('.accordion-container', {
   showMultiple: true,
   duration: 250,
 });
-
 document.querySelector('.faq__list').addEventListener('click', event => {
   const triggerElement = event.target.closest('.ac-trigger');
   if (!triggerElement) return;
-
   const faqItem = triggerElement.closest('.ac');
   const arrowIcon = triggerElement.querySelector('.faq__item-icon use');
   const typedElement = faqItem.querySelector('.typed');
   const stringsElement = faqItem.querySelector('.typed__strings');
-
   arrowIcon.setAttribute(
     'href',
     faqItem.classList.contains('is-active')
       ? `${spriteSvg}#icon-arrow-up`
       : `${spriteSvg}#icon-arrow-down`
   );
-
   if (faqItem.classList.contains('is-active')) {
     if (faqItem.typedInstance) {
       faqItem.typedInstance.stop();
@@ -32,10 +27,8 @@ document.querySelector('.faq__list').addEventListener('click', event => {
     }
     typedElement.innerHTML = '';
   }
-
   if (faqItem.classList.contains('is-active')) {
     typedElement.innerHTML = '';
-
     const typedOptions = {
       typeSpeed: 0,
       startDelay: 250,
@@ -45,11 +38,9 @@ document.querySelector('.faq__list').addEventListener('click', event => {
         faqItem.typedInstance = null;
       },
     };
-
     faqItem.typedInstance = new Typed(typedElement, typedOptions);
   }
 });
-
 document.querySelectorAll('.ac-trigger').forEach(triggerElement => {
   triggerElement.addEventListener('keydown', event => {
     if (event.key === 'Enter') {
